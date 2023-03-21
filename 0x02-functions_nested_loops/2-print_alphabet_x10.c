60825539027066ef68b64e29cd88f6898f0f770a
Omondi Brian <omondiibrian00@gmail.com>
2023-03-21 23:57:02 +0000

    removing error on task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,18 +8,14 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int i = 0;
+	int i;
 
-	while (i <= 9)
+	for (i = 0; i < 10 ; i++)
 	{
-	   for (c = 'a'; c < 'z'; c++)
+		for (c = 'a' ; c <= 'z' ; c++)
 		{
 			_putchar(c);
 		}
-
 		_putchar('\n');
-
-		 i++;
-
 	}
 }
