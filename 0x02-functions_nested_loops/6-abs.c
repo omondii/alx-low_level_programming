ec517bc0782eeb2eac661a4fb9289f35eabce1af
Omondi Brian <omondiibrian00@gmail.com>
2023-03-22 14:44:15 +0000

    Adding _abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an interger
+ *@r: the number to get its absolute value
+ *
+ *Return: If succesfull, return 0 else return 1
+ */
+int _abs(int r)
+{
+	r = _abs(r);
+
+	_putchar(r);
+
+	return (0);
+}
