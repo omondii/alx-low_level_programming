dcf47e5d53eda45e213cac06d6a866143ada5de6
Omondi Brian <omondiibrian00@gmail.com>
2023-03-23 11:25:26 +0000

    Adding 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - multiplies two numbers and prints answer
+ *
+ *@x: Number 1
+ *@y: Number 2
+ *
+ *Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
