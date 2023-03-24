9411e120e529b5c365f97ab8b063ee349b830f28
Omondi Brian <omondiibrian00@gmail.com>
2023-03-24 07:53:22 +0000

    That was difficult, hope this works

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,27 +5,26 @@
  *Return: Always 0
  */
 int main(void)
-int main(void)
 {
-	int i;
-
-	i = 1;
-	while (i <= 100)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%d", i);
-
-		if (i != 100)
-			printf(" ");
-		i++;
-	}
-	printf("\n");
-
-	return (0);
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
 }
