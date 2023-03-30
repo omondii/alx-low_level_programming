5a934c6e84d7c1b45f0eaaf794b8284644408316
Omondi Brian <omondiibrian00@gmail.com>
2023-03-30 21:47:44 +0000

    Look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *string_toupper - changes all lowercase to uppercase
+ *@str: string
+ *Return: always 0
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (int)str[i] - 32;
+	}
+	return (str);
+}
