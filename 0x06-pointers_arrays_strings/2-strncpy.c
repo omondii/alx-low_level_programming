fea85a4a093cb657a994c57307c089f4f3ddd162
Omondi Brian <omondiibrian00@gmail.com>
2023-03-30 20:45:07 +0000

    fixed ***s on first

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *ptr = dest;
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
-	dest[i] = '\0';
 
-	return (ptr);
+	for (; n > i; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
