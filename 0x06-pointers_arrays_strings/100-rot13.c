cb8c03e625ec8546cd070862413b078401f82676
Omondi Brian <omondiibrian00@gmail.com>
2023-03-31 12:28:22 +0000

    Rot-13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,16 +8,14 @@
 char *rot13(char *s)
 {
 	int i;
-	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char storel[] = "nopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
-		{
-			s[i] = (s[i] - 65 > 25) ?
-				storel[s[i] - 97] : storeh[s[i] - 65];
-		}
+		if (*(s + i) >= 'a' && *(s + i) < 'n')
+			*(s + i) += 13;
+
+		else if (*(s + i) >= 'n' && *(s + i) <= 'z')
+			*(s + i) -= 13;
 	}
-	return (s);
+	return (0);
 }
