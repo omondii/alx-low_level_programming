8d4784b5971f34d644f3c10b6a98b33624450edc
Omondi Brian <omondiibrian00@gmail.com>
2023-03-31 12:36:11 +0000

    Rot-13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,11 +11,11 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) < 'n')
-			*(s + i) += 13;
+		if (s[i] >= 'a' && s[i] < 'n')
+			s[i] += 13;
 
-		else if (*(s + i) >= 'n' && *(s + i) <= 'z')
-			*(s + i) -= 13;
+		else if (s[i] >= 'n' && s[i] <= 'z')
+			s[i] -= 13;
 	}
 	return (0);
 }
