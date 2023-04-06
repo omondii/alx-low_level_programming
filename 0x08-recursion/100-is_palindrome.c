a256757ce7bae55631ed33bb526b428f3e147121
Omondii <omondiibrian00@gmail.com>
2023-04-06 19:52:14 +0000

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,15 +4,40 @@
  *@s: string
  *Return: Always 0
  */
-int is_palindrome_helper(char *s, int start, int end)
+int check_palindrome(char *s, int start, int end)
 {
 	if (start >= end)
 	{
-		return 1;
+		return (1);
 	}
-	if (s[start] == s[end])
+	else if (s[start] == s[end])
 	{
-		return (is_palindrome_helper(s, start + 1, end - 1));
+		return check_palindrome(s, start + 1, end - 1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
+}
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	if (len == 0 || len == 1)
+	{
+	return (1);
+	}
+	
+	return (check_palindrome(s, 0, len - 1));
+}
+
+int _strlen_recursion(char *s)
+{
+        if (*s == '\0')
+                return (0);
+        {
+                return ((1) + _strlen_recursion(s + 1));
+        }
+
 }
