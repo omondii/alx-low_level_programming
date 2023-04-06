c51a6f221805014c19048993805f061018dd7025
Omondii <omondiibrian00@gmail.com>
2023-04-06 23:14:37 +0000

    invinsible error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -37,3 +37,19 @@ int is_palindrome(char *s)
 
 	return (check_palindrome(s, 0, len - 1));
 }
+
+/**
+ *_strlen_recursion - returns the length of a string
+ *@s: returns the length of a string
+ *
+ *Return: nothing
+ */
+int _strlen_recursion(char *s)
+{
+        if (*s == '\0')
+                return (0);
+        {
+                return ((1) + _strlen_recursion(s + 1));
+        }
+
+}
