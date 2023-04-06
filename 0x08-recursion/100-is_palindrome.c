0c81491319cd3b18604e6b76e68537f502897441
Omondii <omondiibrian00@gmail.com>
2023-04-06 23:27:45 +0000

    ficing betty

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -32,7 +32,7 @@ int is_palindrome(char *s)
 
 	if (len == 0 || len == 1)
 	{
-	return (1);
+		return (1);
 	}
 
 	return (check_palindrome(s, 0, len - 1));
@@ -46,10 +46,12 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        {
-                return ((1) + _strlen_recursion(s + 1));
-        }
-
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
