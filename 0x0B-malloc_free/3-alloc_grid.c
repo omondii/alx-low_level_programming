3563a52cd7bd59926371e8e16a82aff24881eddd
Omondii <omondiibrian00@gmail.com>
2023-04-12 16:05:41 +0000

    allocating right memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int **grid = malloc(sizeof(int) * height);
+	int **grid = malloc(sizeof(int *) * height);
 
 	if (width <= 0 || height <= 0)
 	{
