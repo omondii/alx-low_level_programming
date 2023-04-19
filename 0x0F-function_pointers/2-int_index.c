046c098eb9b976f273db3c224d20e9853e4e56ab
Omondii <omondiibrian00@gmail.com>
2023-04-19 23:09:49 +0000

    betty fix for 0 to 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,22 +3,21 @@
  *int_index - searchers for an interger
  *@array: array where intergers are checked
  *@size: size of the array
- *@cmp: functio that checks intergers
+ *@cmp: function that checks intergers
+ *Return: if no match -1 else i
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (size <= 0)
-	{
 		return (-1);
-	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			{
-				return (i);
-			}
+		{
+			return (i);
+		}
 	}
 		return (-1);
 }
