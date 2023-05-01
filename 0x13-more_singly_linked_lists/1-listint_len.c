4b67e01b73cda1a8507cdc8a9182f05aed3cd299
Brian Omondi <omondiibrian00@gmail.com>
2023-05-01 19:08:51 +0300

    printing the no. of elements in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+/**
+ *listint_len - prints the number of elements in a linked list
+ *@h: head of a list
+ *Return: count
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
