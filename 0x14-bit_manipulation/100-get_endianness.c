481ff424d425309020b92b7827a8b51714d16b9d
Brian Omondi <omondiibrian00@gmail.com>
2023-05-05 01:33:48 +0300

    fix betty

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,13 +2,12 @@
 /**
  *get_endianness - gets the order in which data types are stored
  * in computer memory
- *@word: word to check
- *@byte: allows each character access
- *Returns: if big endian (0)if little endian(1)
+ *Return: if big endian (0)if little endian(1)
  */
 int get_endianness(void)
 {
 	short int word = 0x0001;
 	char *byte = (char *) &word;
+
 	return (byte[0] ? 1 : 0);
 }
