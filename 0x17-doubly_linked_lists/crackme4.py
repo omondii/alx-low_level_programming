59bd6e0053ed27dbc50d975320bbe77a895ef9e3
Brian Omondi <omondiibrian00@gmail.com>
2023-06-17 19:26:44 +0300

    cracked crackme4

---
+++ 0x17-doubly_linked_lists/crackme4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+pwd = input ('password: ')
+ok = 'Zen of python'
+ok = ok + ' C'
+ok = ok[1:3] + ok[3] + ok[14] + ok[3] + ok[7:9] + ok[:6][::-1]
+print (ok)
+if pwd == ok:
+    print('OK')
+else:
+    print('KO')
