5c4b8c1a2199973e9e92e374a8ecc4a473079618
Brian Omondi <omondiibrian00@gmail.com>
2023-07-08 00:29:35 +0300

    djb2 hash function implementation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,6 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(int));
+
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
