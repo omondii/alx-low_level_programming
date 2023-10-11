5ed204fbed4ec02a2c9d27ca4c1999535c2a964c
Brian Omondi <omondiibrian00@gmail.com>
2023-10-11 21:13:10 +0300

    betty fix 1.1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,12 +12,11 @@ int linear_search(int *array, size_t size, int value)
 
 	if (size == 0)
 		return (-1);
-	// check of the current value of array, if it equals value. return it
+
 	if (array[size - 1] == value)
 	{
 		return (size - 1);
 	}
-	// Recursively iterate over the array
 	printf("Value checked array[%lu] = [%d]\n", size - 1, array[size - 1]);
 	return linear_search(array, size - 1, value);
 }
